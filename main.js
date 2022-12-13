4cb9b68fcf1f0816b372151f02b29064cdc8e5a9
Joshua Scott <scottjm@nsuok.edu>
2022-12-13 10:35:09 -0600

    First function complete.

---
+++ main.js
@@ -0,0 +1,15 @@
+function getComputerChoice(){
+    let things = ["Rock", "Paper", "Scissor"];
+    let thing = things[Math.floor(Math.random()*things.length)];
+    alert('The computer chose: ' + thing);}
+    getComputerChoice()
+
+let choice = prompt("Pick Rock, Paper, or Scissor")
+
+if (choice = thing){
+    "It's a draw"}
+
+
+
+
+
