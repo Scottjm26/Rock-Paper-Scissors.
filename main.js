1419950d34ad6f4ed0e1dbc23073fb00d5546b28
Joshua Scott <scottjm@nsuok.edu>
2022-12-13 17:26:44 -0600

    Finished playgame function, working next function

--- main.js
+++ main.js
@@ -1,20 +1,27 @@
 function getComputerChoice(){
-let things = ["Rock", "Paper", "Scissor"];
+let things = ["rock", "paper", "scissors"];
 let thing = things[Math.floor(Math.random()*things.length)];
+alert (thing)
 return thing;}
-getComputerChoice()
+
 
 function playGame(playerSelection, computerSelection){
-    if (playerSelection == "rock") and (computerSelection == "scissor")
-    {return "Computer loses";}
-    if (playerSelection == "paper") and (computerSelection == "rock")
-    {return "Computer loses"}
-    if (playerSelection == "scissors") and (computerSelection == "paper")
-    {return "Computer loses"}
-
-    let playerSelection = "rock"
+    if (playerSelection == computerSelection)
+    alert ("Tied ")
+    else if (playerSelection == "rock" && computerSelection == "scissors")
+    alert (("You win "+ playerSelection+ " beats " +computerSelection));
+    else if (playerSelection == "paper" && computerSelection == "rock")
+    alert (("You win "+ playerSelection+ " beats " +computerSelection))
+    else if (playerSelection == "scissors" && computerSelection == "paper")
+    alert (("You win "+ playerSelection+ " beats " +computerSelection))
+    else 
+    alert ("You lose "+ computerSelection + " beats " + playerSelection) }
+    
+    let playerSelection = prompt("rock, paper, or scissors")
     let computerSelection = getComputerChoice();
-    console.log(playGame(playerSelection, computerSelection))}
+    console.log(playGame(playerSelection, computerSelection))
 
 
-
+function game(){
+for (let i =0;i<5;i++) 
+playGame(i)}
