36cb2237537f172f7b4f2e3e57901f49a878f477
Joshua Scott <scottjm@nsuok.edu>
2022-12-13 11:57:26 -0600

    Working on second function

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 function getComputerChoice(){
-    let things = ["Rock", "Paper", "Scissor"];
-    let thing = things[Math.floor(Math.random()*things.length)];
-    alert('The computer chose: ' + thing);}
-    getComputerChoice()
+let things = ["Rock", "Paper", "Scissor"];
+let thing = things[Math.floor(Math.random()*things.length)];
+return thing;}
+getComputerChoice()
 
-let choice = prompt("Pick Rock, Paper, or Scissor")
+function playGame(playerSelection, computerSelection){
+    if (playerSelection == "rock") and (computerSelection == "scissor")
+    {return "Computer loses";}
+    if (playerSelection == "paper") and (computerSelection == "rock")
+    {return "Computer loses"}
+    if (playerSelection == "scissors") and (computerSelection == "paper")
+    {return "Computer loses"}
 
-if (choice = thing){
-    "It's a draw"}
-
-
+    let playerSelection = "rock"
+    let computerSelection = getComputerChoice();
+    console.log(playGame(playerSelection, computerSelection))}
 
 
 
