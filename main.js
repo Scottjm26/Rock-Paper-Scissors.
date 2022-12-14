fe79fd2931833250c49cc6c79f342846c61ff05d
Joshua Scott <scottjm@nsuok.edu>
2022-12-13 21:06:55 -0600

    made little progess

--- main.js
+++ main.js
@@ -9,19 +9,21 @@ function playGame(playerSelection, computerSelection){
     if (playerSelection == computerSelection)
     alert ("Tied ")
     else if (playerSelection == "rock" && computerSelection == "scissors")
-    alert (("You win "+ playerSelection+ " beats " +computerSelection));
+    (playerPoints++)("You win "+ playerSelection+ " beats " +computerSelection);
     else if (playerSelection == "paper" && computerSelection == "rock")
-    alert (("You win "+ playerSelection+ " beats " +computerSelection))
+    (playerPoints++)("You win "+ playerSelection+ " beats " +computerSelection)
     else if (playerSelection == "scissors" && computerSelection == "paper")
-    alert (("You win "+ playerSelection+ " beats " +computerSelection))
+    (playerPoints++) ("You win "+ playerSelection+ " beats " +computerSelection)
     else 
-    alert ("You lose "+ computerSelection + " beats " + playerSelection) }
+    (computerPoints++) (alert ("You lose "+ computerSelection + " beats " + playerSelection)) }
     
     let playerSelection = prompt("rock, paper, or scissors")
     let computerSelection = getComputerChoice();
-    console.log(playGame(playerSelection, computerSelection))
+    playGame(playerSelection, computerSelection)
 
 
-function game(){
-for (let i =0;i<5;i++) 
-playGame(i)}
+function game()
+    let playerPoints = 0
+    let computerPoints = 0
+    for (let i = 0; i < 5; i++)
+game()
