7cc3e556e9d8d33fc3c99ccf75ba18299fe4c7e6
Joshua Scott <scottjm@nsuok.edu>
2022-12-14 20:10:51 -0600

    Finished project. For now...

--- main.js
+++ main.js
@@ -1,29 +1,50 @@
+
+let playerPoints = 0;
+let computerPoints = 0;
+
 function getComputerChoice(){
-let things = ["rock", "paper", "scissors"];
-let thing = things[Math.floor(Math.random()*things.length)];
-alert (thing)
-return thing;}
-
-
-function playGame(playerSelection, computerSelection){
-    if (playerSelection == computerSelection)
-    alert ("Tied ")
-    else if (playerSelection == "rock" && computerSelection == "scissors")
-    (playerPoints++)("You win "+ playerSelection+ " beats " +computerSelection);
-    else if (playerSelection == "paper" && computerSelection == "rock")
-    (playerPoints++)("You win "+ playerSelection+ " beats " +computerSelection)
-    else if (playerSelection == "scissors" && computerSelection == "paper")
-    (playerPoints++) ("You win "+ playerSelection+ " beats " +computerSelection)
-    else 
-    (computerPoints++) (alert ("You lose "+ computerSelection + " beats " + playerSelection)) }
+    let things = ["rock", "paper", "scissors"];
+    let thing = things[Math.floor(Math.random()*things.length)];
+    alert (thing)
+    return thing;}
     
-    let playerSelection = prompt("rock, paper, or scissors")
+function playGame(playerSelection, computerSelection){
+    if (playerSelection == computerSelection){
+        alert ("Tied")}
+    else if (playerSelection == "rock" && computerSelection == "scissors"){
+        ++playerPoints;
+        alert ("You win this round "+ playerSelection+ " beats " +computerSelection)}
+    else if (playerSelection == "paper" && computerSelection == "rock"){
+        ++playerPoints;
+        alert ("You win this round "+ playerSelection+ " beats " +computerSelection)}
+    else if (playerSelection == "scissors" && computerSelection == "paper"){
+        ++playerPoints;
+        alert ("You win this round "+ playerSelection+ " beats " +computerSelection)}
+    else {
+        ++computerPoints;
+        alert("You lose this round "+ computerSelection + " beats " + playerSelection)}
+    }
+        
+function game(){
+    for(let i=0; i < 5; i++) {
+    let playerSelection = prompt("Rock, Paper, or Scissors".toLowerCase())
     let computerSelection = getComputerChoice();
-    playGame(playerSelection, computerSelection)
-
-
-function game()
-    let playerPoints = 0
-    let computerPoints = 0
-    for (let i = 0; i < 5; i++)
+    playGame(playerSelection, computerSelection)}}
 game()
+    
+console.log(playerPoints)
+console.log(computerPoints)
+if (playerPoints > computerPoints){
+    alert("You are the winner!")}
+else if (computerPoints > playerPoints){
+    alert("The computer whooped your ass!")}
+else
+    {alert("You Tied!")}
+
+    
+    
+    
+
+
+   
+        
